d7bfaa65cf79edde3f928903d2b4ca7388b09db0
Edward Kesicki <edkesicki@gmail.com>
2018-10-09 13:16:38 -0700

    Added test files

---
+++ CARS/CARS-test.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) AS CarNames FROM CarNames;
+SELECT COUNT(*) AS CarMakers FROM CarMakers;
+SELECT COUNT(*) AS CarsData FROM CarsData;
+SELECT COUNT(*) AS Continents FROM Continents;
+SELECT COUNT(*) AS Countries FROM Countries;
+SELECT COUNT(*) AS ModelList FROM ModelList;
---
+++ MARATHON/MARATHON-test.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(*) AS Marathon from Marathon;
