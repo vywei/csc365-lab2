90cab0b4c94ee1b8519d4e3b4ae6d7f4f9f61da5
Victor <vwei97@gmail.com>
2018-10-08 23:34:20 -0700

    changed python script to remove trailing spaces

--- CSU/CSVToSQL.py
+++ CSU/CSVToSQL.py
@@ -4,6 +4,7 @@ import sys
 def main():
     if len(sys.argv) != 4:
         print "Usage: python convertToSQL.py <csv input file name> <table name> <sql output file name>\n **No filename Extensions**"
+        return
     csvName = sys.argv[1]
     tableName = sys.argv[2]
     sqlName = sys.argv[3]
@@ -14,6 +15,9 @@ def main():
         readCSV = csv.reader(csvfile, delimiter=',')
         key = next(readCSV)
         for row in readCSV:
+            for i in xrange(len(row)):
+                if row[i][0] == ("\'") and row[i][-1] == ("\'"):
+                    row[i] = "\'" + row[i][1:-1].rstrip() + "\'"
             sqlFile.write("INSERT INTO %s (" % tableName + ', '.join(key) + ") \n    VALUES (" + ', '.join(row) + ");\n")
 
     sqlFile.close()
