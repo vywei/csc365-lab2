cd09c5e7ab7bfe042f76c8abb0b59fc4a3135404
Victor Wei <vywei@unix2.csc.calpoly.edu>
2018-10-08 21:25:12 -0700

    CSU v1.1

--- CSU/CSU-setup.sql
+++ CSU/CSU-setup.sql
@@ -1,49 +1,44 @@
 CREATE TABLE campuses (
-    campusID INT,
-    campusName VARCHAR(50),
-    location VARCHAR(50),
-    county VARCHAR(50),
-    year INT,
-    PRIMARY KEY (campusID));
+    Id INT,
+    Campus VARCHAR(60),
+    Location VARCHAR(50),
+    County VARCHAR(50),
+    Year INT,
+    PRIMARY KEY (Id));
 
 CREATE TABLE csuFees (
-    campusID INT,
-    year INT,
-    campusFee INT,
-    PRIMARY KEY (campusID, year)),
-    FOREIGN KEY (campusID) references campuses(campusID) ON DELETE CASCADE,
-    UNIQUE (campusID));
+    Campus INT references campuses(Id),
+    Year INT,
+    CampusFee INT,
+    PRIMARY KEY (Campus, Year));
 
 CREATE TABLE degrees (
-    campusID INT, 
-    year INT,
-    numDegrees INT,
-    PRIMARY KEY (campusID, year)
-    FOREIGN KEY (campusID) references campuses(campusID) ON DELETE CASCADE);
+    Campus INT references campuses(Id) ON DELETE CASCADE, 
+    Year INT,
+    Degrees INT,
+    PRIMARY KEY (Campus, Year));
 
 CREATE TABLE disciplineEnrollments (
-    campusID INT,
-    disciplineID INT,
-    year INT,
-    numUndergrads INT,
-    numGrads INT,--number of currently enrolled graduate students
-    PRIMARY KEY (campusID, disciplineID),
-    FOREIGN KEY (campusID) references campuses(campusID) ON DELETE CASCADE,
-    FOREIGN KEY (disciplineID) references disciplines(disciplineID) ON DELETE CASCADE);
+    Campus INT references campuses(Id) ON DELETE CASCADE,
+    Discipline INT references disciplines(Id) ON DELETE CASCADE,
+    Year INT,
+    Undergraduate INT,
+    Graduate INT,
+    PRIMARY KEY (Campus, Discipline));
 
 CREATE TABLE disciplines (
-    disciplineID INT PRIMARY KEY,
-    disciplineName VARCHAR(50));
+    Id INT PRIMARY KEY,
+    Name VARCHAR(50));
 
 CREATE TABLE enrollments (
-    campusID INT,
-    year INT,
-    totalEnrollmentForYear INT,
-    fullTimeEnrollmentForYear INT,
-    FOREIGN KEY (campusID) references campuses(campusID) ON DELETE CASCADE);
+    Campus INT,
+    Year INT,
+    TotalEnrollment_AY INT,
+    FTE_AY INT,
+    FOREIGN KEY (Campus) references campuses(Id) ON DELETE CASCADE);
 
 CREATE TABLE faculty (
-    campusID INT,
-    year INT,
-    numFaculty INT,
-    FOREIGN KEY (campusID) references campuses(campusID) ON DELETE CASCADE);
+    Campus INT,
+    Year INT,
+    Faculty INT,
+    FOREIGN KEY (Campus) references campuses(Id) ON DELETE CASCADE);
