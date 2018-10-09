bb67ac6744a11ddcd66fcd6437c9a1d878fd073a
Edward Kesicki <edkesicki@gmail.com>
2018-10-09 00:59:08 -0700

    Added Cars, car-names still needs some work

--- CSU/CSVToSQL.py
+++ CSU/CSVToSQL.py
@@ -2,12 +2,10 @@ import csv
 import sys
 
 def main():
-    if len(sys.argv) != 4:
-        print "Usage: python convertToSQL.py <csv input file name> <table name> <sql output file name>\n **No filename Extensions**"
-        return
+    if len(sys.argv) != 3:
+        print "Usage: python convertToSQL.py <csv input file name> <sql output file name>\n **No filename Extensions**"
     csvName = sys.argv[1]
-    tableName = sys.argv[2]
-    sqlName = sys.argv[3]
+    sqlName = sys.argv[2]
     sqlFile = open("%s.sql" % sqlName, "w")
 
 
@@ -15,10 +13,7 @@ def main():
         readCSV = csv.reader(csvfile, delimiter=',')
         key = next(readCSV)
         for row in readCSV:
-            for i in xrange(len(row)):
-                if row[i][0] == ("\'") and row[i][-1] == ("\'"):
-                    row[i] = "\'" + row[i][1:-1].rstrip() + "\'"
-            sqlFile.write("INSERT INTO %s (" % tableName + ', '.join(key) + ") \n    VALUES (" + ', '.join(row) + ");\n")
+            sqlFile.write("INSERT INTO %s (" % sqlName + ', '.join(key) + ") \n    VALUES (" + ', '.join(row) + ");\n")
 
     sqlFile.close()
 
