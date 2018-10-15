d1e99c98e9c5ba55deca12d3b134c39bdd2ea575
Victor Wei <vywei@Victors-MacBook.local>
2018-10-15 13:26:57 -0700

    refactoring

---
+++ lab3/MARATHON/Marathon-Setup.sql
@@ -0,0 +1,20 @@
+/*
+Victor Wei - vywei@calpoly.edu
+Eddie Kesicki - ekesicki@calpoly.edu
+*/
+
+CREATE TABLE Marathon (
+    Place INT,
+    mTime TIME,
+    Pace TIME,    
+    GroupPlace INT,
+    mGroup VARCHAR(20),
+    Age VARCHAR(10),
+    Sex VARCHAR(2),
+    BIBNumber INT,
+    FirstName VARCHAR(20),
+    LastName VARCHAR(20),
+    Town VARCHAR(20),
+    mState VARCHAR(20),
+    PRIMARY KEY (Place)
+);
