dafd0cafc14d1ccc652e7019bc4e17ee68419a89
vywei <vywei@unix2.csc.calpoly.edu>
2018-10-17 13:19:10 -0700

    CARS

---
+++ lab3/CARS/CARS-insert.sql
@@ -0,0 +1,6 @@
+source CARS-build-car-makers.sql
+source CARS-build-car-names.sql
+source CARS-build-cars-data.sql
+source CARS-build-continents.sql
+source CARS-build-countries.sql
+source CARS-build-model-list.sql
